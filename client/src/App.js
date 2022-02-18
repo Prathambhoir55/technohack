bec6e0aafae5292c26586c473f2502648ed2b27c
lokita282 <lokitavarma282@gmail.com>
2022-02-18 18:08:32 +0530

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import SignUp from './components/SignUp'
+
+function App() {
+  return (
+    <div className="App">
+      <b>hi</b>
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
