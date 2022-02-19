853481f54c9075b83a6602e7e3b34fa9a72bcd3d
lokita282 <lokitavarma282@gmail.com>
2022-02-19 13:04:01 +0530

    added remaining components and their routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,28 @@ import { Route, Routes } from "react-router";
 import SignIn from './components/SignIn'
 import Landing from './components/landing/Landing'
 import Dashboard from './components/Dashboard'
-import SideBar from './components/sideBar'
+import SideBar from './components/SideBar'
+import Shop from './components/Shop'
+import Events from './components/Events'
+import Learn from './components/Learn'
+import Tutorials from './components/Tutorials'
+
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/events' element={<SideBar/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/events" element={<SideBar />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/learn" element={<Learn />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
